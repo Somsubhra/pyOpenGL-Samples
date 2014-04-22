02da2047aefecf1f8d1e66bc5e50cf46a8e3d06a
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-04-22 17:33:01 +0530

    OOPize

--- humanoid.py
+++ humanoid.py
@@ -1,82 +1,90 @@
 # OpenGL imports for python
-from OpenGL.GL import *
-from OpenGL.GLU import *
-from OpenGL.GLUT import *
+
+try:
+    from OpenGL.GL import *
+    from OpenGL.GLU import *
+    from OpenGL.GLUT import *
+except:
+    print "OpenGL wrapper for python not found"
+
+class Humanoid:
+
+    def __init__(self):
+        # Head angle horizontally(left/right)
+        self.head_angle_lr = 0
+
+        # Head angle vertically(up/down)
+        self.head_angle_ud = 0
+
+        # Neck angle horizontally
+        self.neck_angle_lr = 0
+
+        # Neck angle vertically
+        self.neck_angle_ud = 0
+
+        # Right Shoulder angle horizontally(left/right)
+        self.right_shoulder_angle_lr = 0
+
+        # Right Shoulder angle vertically(up/down)
+        self.right_shoulder_angle_ud = 0
+
+        # Left Shoulder angle horizontally(left/right)
+        self.left_shoulder_angle_lr = 0
+
+        # Left Shoulder angle vertically(up/down)
+        self.left_shoulder_angle_ud = 0
+
+        # Right Ankle angle vertically
+        self.right_ankle_angle_ud = 0
+
+        # Left Ankle angle vertically
+        self.left_ankle_angle_ud = 0
+
+        # Torso angle vertically(up/down)
+        self.torso_angle_ud = 0
+
+        # Torso angle horizontally(left/right)
+        self.torso_angle_lr = 0
+
+        # Left leg angle vertically(up/down)
+        self.left_leg_ud = 0
+
+        # Right leg angle vertically(up/down)
+        self.right_leg_ud = 0
+
+        # Left knee angle vertically(up/down)
+        self.left_knee_ud = 0
+
+        # Right knee angle vertically(up/down)
+        self.right_knee_ud = 0
+
+    def display(self):
+        glEnable(GL_DEPTH_TEST)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+
+    def controls(self):
+        pass
 
 
-# Head angle horizontally(left/right)
-head_angle_lr = 0
+def main():
+    glutInit(sys.argv)
 
-# Head angle vertically(up/down)
-head_angle_ud = 0
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
 
-# Neck angle horizontally
-neck_angle_lr = 0
+    glutInitWindowSize(600, 500)
+    glutInitWindowPosition(0, 0)
 
-# Neck angle vertically
-neck_angle_ud = 0
+    glutCreateWindow('Humanoid')
 
-# Right Shoulder angle horizontally(left/right)
-right_shoulder_angle_lr = 0
+    h = Humanoid()
 
-# Right Shoulder angle vertically(up/down)
-right_shoulder_angle_ud = 0
+    glutDisplayFunc(h.display)
+    glutIdleFunc(h.display)
+    glutKeyboardFunc(h.controls())
 
-# Left Shoulder angle horizontally(left/right)
-left_shoulder_angle_lr = 0
+    glutMainLoop()
 
-# Left Shoulder angle vertically(up/down)
-left_shoulder_angle_ud = 0
-
-# Right Ankle angle vertically
-right_ankle_angle_ud = 0
-
-# Left Ankle angle vertically
-left_ankle_angle_ud = 0
-
-# Torso angle vertically(up/down)
-torso_angle_ud = 0
-
-# Torso angle horizontally(left/right)
-torso_angle_lr = 0
-
-# Left leg angle vertically(up/down)
-left_leg_ud = 0
-
-# Right leg angle vertically(up/down)
-right_leg_ud = 0
-
-# Left knee angle vertically(up/down)
-left_knee_ud = 0
-
-# Right knee angle vertically(up/down)
-right_knee_ud = 0
-
-
-def display_humanoid():
-
-    glEnable(GL_DEPTH_TEST)
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
-    glMatrixMode(GL_MODELVIEW)
-
-    glLoadIdentity()
-
-
-def keyboard_controls():
-    pass
-
-glutInit(sys.argv)
-
-glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
-
-glutInitWindowSize(600, 500)
-glutInitWindowPosition(0, 0)
-
-glutCreateWindow('Humanoid')
-
-glutDisplayFunc(display_humanoid)
-glutIdleFunc(display_humanoid)
-glutKeyboardFunc(keyboard_controls)
-
-glutMainLoop()
+if __name__ == '__main__':
+    main()
