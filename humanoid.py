00ad70193ffb7aa80347058d3fa8a929fd24d4b0
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-04-22 17:38:35 +0530

    Add DOX every other line.

--- humanoid.py
+++ humanoid.py
@@ -1,5 +1,4 @@
 # OpenGL imports for python
-
 try:
     from OpenGL.GL import *
     from OpenGL.GLU import *
@@ -7,8 +6,11 @@ try:
 except:
     print "OpenGL wrapper for python not found"
 
+
+# The humanoid class
 class Humanoid:
 
+    # Constructor for the humanoid class
     def __init__(self):
         # Head angle horizontally(left/right)
         self.head_angle_lr = 0
@@ -58,33 +60,55 @@ class Humanoid:
         # Right knee angle vertically(up/down)
         self.right_knee_ud = 0
 
+    # The display function for the humanoid
     def display(self):
+        # Enable the Depth test
         glEnable(GL_DEPTH_TEST)
+
+        # Set OpenGL parameters
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        # Set the matrix mode to ModelView
         glMatrixMode(GL_MODELVIEW)
+
+        # Load the identity matrix
         glLoadIdentity()
 
+    # Te keyboard controls for the humanoid
     def controls(self):
         pass
 
 
 def main():
+
+    # Initialize the OpenGL pipeline
     glutInit(sys.argv)
 
+    # Set OpenGL display mode
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
 
+    # Set the Window size and position
     glutInitWindowSize(600, 500)
     glutInitWindowPosition(0, 0)
 
+    # Create the window with given title
     glutCreateWindow('Humanoid')
 
+    # Instantiate the humanoid
     h = Humanoid()
 
+    # Attach the callback for the display function
     glutDisplayFunc(h.display)
-    glutIdleFunc(h.display)
-    glutKeyboardFunc(h.controls())
 
+    # Attach the callback for the idle function
+    glutIdleFunc(h.display)
+
+    # Attach the callback function for keyboard functions
+    glutKeyboardFunc(h.controls)
+
+    # Run the OpenGL main loop
     glutMainLoop()
 
+# Call the main function
 if __name__ == '__main__':
     main()
