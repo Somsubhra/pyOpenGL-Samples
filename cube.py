d7f6bda48bd054055c1ec96b8716cd8906d5fae2
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-04-25 00:29:58 +0530

    Reduce code redundancy

--- cube.py
+++ cube.py
@@ -19,10 +19,7 @@ class Cube:
         glClearColor(0.0, 0.0, 0.0, 0.0)
         glShadeModel(GL_FLAT)
 
-    # The display function
-    def display(self):
-        glClear(GL_COLOR_BUFFER_BIT)
-
+    def draw_half(self, mirror):
         eqn = [-1.0, 0.0, 0.0, 0.0]
         eqn1 = [1.0, 1.0, 1.0, 1.25]
         eqn2 = [1.0, -1.0, 1.0, 1.25]
@@ -34,8 +31,13 @@ class Cube:
         glLoadIdentity()
         gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
 
-        glScalef(self.scale, self.scale, self.scale)
-        glRotatef(self.rotate_y, 0.0, 1.0, 0.0)
+        if mirror:
+            glScalef(-self.scale, self.scale, self.scale)
+            glRotatef(-self.rotate_y, 0.0, 1.0, 0.0)
+        else:
+            glScalef(self.scale, self.scale, self.scale)
+            glRotatef(self.rotate_y, 0.0, 1.0, 0.0)
+
         glRotatef(self.rotate_x, 1.0, 0.0, 0.0)
 
         glutSolidCube(1.0)
@@ -63,38 +65,11 @@ class Cube:
 
         glFlush()
 
-        glColor3f(1.0, 1.0, 1.0)
-        glLoadIdentity()
-        gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
-
-        glScalef(-self.scale, self.scale, self.scale)
-        glRotatef(-self.rotate_y, 0.0, 1.0, 0.0)
-        glRotatef(self.rotate_x, 1.0, 0.0, 0.0)
-
-        glutSolidCube(1.0)
-
-        glColor3f(1.0, 0.0, 0.0)
-        glutWireCube(1.0)
-
-        glClipPlane(GL_CLIP_PLANE0, eqn1)
-        glEnable(GL_CLIP_PLANE0)
-
-        glClipPlane(GL_CLIP_PLANE1, eqn2)
-        glEnable(GL_CLIP_PLANE1)
-
-        glClipPlane(GL_CLIP_PLANE2, eqn3)
-        glEnable(GL_CLIP_PLANE2)
-
-        glClipPlane(GL_CLIP_PLANE3, eqn4)
-        glEnable(GL_CLIP_PLANE3)
-
-        glClipPlane(GL_CLIP_PLANE4, eqn5)
-        glEnable(GL_CLIP_PLANE4)
-
-        glClipPlane(GL_CLIP_PLANE5, eqn)
-        glEnable(GL_CLIP_PLANE5)
-
-        glFlush()
+    # The display function
+    def display(self):
+        glClear(GL_COLOR_BUFFER_BIT)
+        self.draw_half(False)
+        self.draw_half(True)
 
     # The reshape function
     def reshape(self, w, h):
