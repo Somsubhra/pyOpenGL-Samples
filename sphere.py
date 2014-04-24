08df0513db8568f151933a2dc168442442ca046f
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-04-24 14:54:16 +0530

    Save sphere.py

--- sphere.py
+++ sphere.py
@@ -11,144 +11,103 @@ except:
     print "OpenGL wrapper for python not found"
 
 
+last_time = 0
+
+
 # The sphere class
 class Sphere:
 
     # Constructor for the sphere class
     def __init__(self, radius):
-
-        # The radius of the sphere
         self.radius = radius
+        self.lats = 10
+        self.longs = 10
+        self.user_theta = 0
+        self.user_height = 0
+        self.direction = [0.0, 2.0, -1.0, 1.0]
+        self.intensity = [0.7, 0.7, 0.7, 1.0]
+        self.ambient_intensity = [0.3, 0.3, 0.3, 1.0]
 
-        self.x = 0.525731112119133606
-        self.z = 0.850650808352039932
+    def init(self):
+        glClearColor(1.0, 1.0, 1.0, 0.0)
+        self.compute_location()
+        glEnable(GL_DEPTH_TEST)
+        glEnable(GL_LIGHTING)
+        glLightModelfv(GL_LIGHT_MODEL_AMBIENT, self.ambient_intensity)
+        glEnable(GL_LIGHT0)
+        glLightfv(GL_LIGHT0, GL_POSITION, self.direction)
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, self.intensity)
+        glEnable(GL_COLOR_MATERIAL)
+        glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE)
 
-        self.vdata = [
-            [-self.x, 0.0, self.z],
-            [self.x, 0.0, self.z],
-            [-self.x, 0.0, -self.z],
-            [self.x, 0.0, -self.z],
-            [0.0, self.z, self.x],
-            [0.0, self.z, -self.x],
-            [0.0, -self.z, self.x],
-            [0.0, -self.z, -self.x],
-            [self.z, self.x, 0.0],
-            [-self.z, self.x, 0.0],
-            [self.z, -self.x, 0.0],
-            [-self.z, -self.x, 0.0]
-        ]
+    def compute_location(self):
+        x = 2 * cos(self.user_theta)
+        y = 2 * sin(self.user_theta)
+        z = self.user_height
+        d = sqrt(x * x + y * y + z * z)
 
-        self.tindices = [
-            [0, 4, 1],
-            [0, 9, 4],
-            [9, 5, 4],
-            [4, 5, 8],
-            [4, 8, 1],
-            [8, 10, 1],
-            [8, 3, 10],
-            [5, 3, 8],
-            [5, 2, 3],
-            [2, 7, 3],
-            [7, 10, 3],
-            [7, 6, 10],
-            [7, 11, 6],
-            [11, 0, 6],
-            [0, 1, 6],
-            [6, 1, 10],
-            [9, 0, 11],
-            [9, 11, 2],
-            [9, 2, 5],
-            [7, 2, 11]
-        ]
-
-        self.divs = 10
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glFrustum(-d * 0.5, d * 0.5, -d * 0.5, d * 0.5, d - 1.1, d + 1.1)
+        gluLookAt(x, y, z, 0, 0, 0, 0, 0, 1)
 
     # Display the sphere
     def display(self):
-
-        glClear(GL_COLOR_BUFFER_BIT)
-
-        glPushMatrix()
-
-        # Draw the sphere
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glColor3f(1.0, 1.0, 1.0)
+        glShadeModel(GL_SMOOTH)
         self.draw()
-
-        glPopMatrix()
-
         glutSwapBuffers()
-
-    # The reshape callback
-    def reshape(self, w, h):
-
-        glViewport(0, 0, w, h)
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        gluPerspective(60, w / h, 1.0, 20.0)
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
-        gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0)
 
     # Draw the sphere
     def draw(self):
+        for i in range(0, self.lats + 1):
+            lat0 = pi * (-0.5 + float((i - 1) / self.lats))
+            z0 = sin(lat0)
+            zr0 = cos(lat0)
 
-        glBegin(GL_TRIANGLES)
+            lat1 = pi * (-0.5 + float(i / self.lats))
+            z1 = sin(lat1)
+            zr1 = cos(lat1)
 
-        for i in range(0, 20):
-            x = self.tindices[i][0]
-            y = self.tindices[i][1]
-            z = self.tindices[i][2]
-            self.draw_triangle(self.vdata[x], self.vdata[y], self.vdata[z], self.divs, self.radius)
+            glBegin(GL_QUAD_STRIP)
 
-        glEnd()
+            for j in range(0, self.longs + 1):
+                lng = 2 * pi * float((j - 1) / self.longs)
+                x = cos(lng)
+                y = sin(lng)
+                glNormal3f(x * zr0, y * zr0, z0)
+                glVertex3f(x * zr0, y * zr0, z0)
+                glNormal3f(x * zr1, y * zr1, z1)
+                glVertex3f(x * zr1, y * zr1, z1)
 
-    # Draw the triangle
-    def draw_triangle(self, m, n, o, div, r):
+            glEnd()
 
-        if div <= 0:
-
-            glNormal3fv(m)
-            glVertex3f(m[0] * r, m[1] * r, m[2] * r)
-
-            glNormal3fv(n)
-            glVertex3f(n[0] * r, n[1] * r, n[2] * r)
-
-            glNormal3fv(o)
-            glVertex3f(o[0] * r, o[1] * r, o[2] * r)
-
-        else:
-
-            mn = [0.0, 0.0, 0.0]
-            no = [0.0, 0.0, 0.0]
-            mo = [0.0, 0.0, 0.0]
-
-            for i in range(0, 3):
-                mn[i] = (m[i] + n[i]) / 2
-                no[i] = (n[i] + o[i]) / 2
-                mo[i] = (m[i] + o[i]) / 2
-
-            self.normalize(mn)
-            self.normalize(no)
-            self.normalize(mo)
-
-            self.draw_triangle(m, mn, mo, div - 1, r)
-            self.draw_triangle(n, no, mn, div - 1, r)
-            self.draw_triangle(o, mo, no, div - 1, r)
-            self.draw_triangle(mn, no, mo, div - 1, r)
-
-    # Normalize a vector
-    def normalize(self, a):
-
-        magnitude = sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2])
-        a[0] /= magnitude
-        a[1] /= magnitude
-        a[2] /= magnitude
-
-    # Keyboard controls
-    def control(self, key, x, y):
-        if key == chr(27):
-            sys.exit()
-
+    def special(self, key, x, y):
+        if key == GLUT_KEY_UP:
+            self.user_height += 0.1
+        if key == GLUT_KEY_DOWN:
+            self.user_height -= 0.1
+        if key == GLUT_KEY_LEFT:
+            self.user_theta += 0.1
+        if key == GLUT_KEY_RIGHT:
+            self.user_theta -= 0.1
+        self.compute_location()
         glutPostRedisplay()
+
+    def idle(self):
+        global last_time
+        time = glutGet(GLUT_ELAPSED_TIME)
+
+        if last_time == 0 or time >= last_time + 40:
+            last_time = time
+            glutPostRedisplay()
+
+    def visible(self, vis):
+        if vis == GLUT_VISIBLE:
+            glutIdleFunc(self.idle)
+        else:
+            glutIdleFunc(None)
 
 
 # The main function
@@ -158,32 +117,28 @@ def main():
     glutInit(sys.argv)
 
     # Set OpenGL display mode
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE)
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH)
 
     # Set the Window size and position
-    glutInitWindowSize(600, 500)
-    glutInitWindowPosition(0, 0)
+    glutInitWindowSize(300, 300)
+    glutInitWindowPosition(50, 100)
 
     # Create the window with given title
     glutCreateWindow('Sphere')
 
-    # Set background color to black
-    glClearColor(0.0, 0.0, 0.0, 0.0)
-
-    # Set shade model
-    glShadeModel(GL_SMOOTH)
-
     # Instantiate the sphere object
     s = Sphere(1.0)
+
+    s.init()
 
     # Set the callback function for display
     glutDisplayFunc(s.display)
 
-    # Set the callback function for reshape
-    glutReshapeFunc(s.reshape)
+    # Set the callback function for the visibility
+    glutVisibilityFunc(s.visible)
 
-    # Set the callback function for keyboard control
-    glutKeyboardFunc(s.control)
+    # Set the callback for special function
+    glutSpecialFunc(s.special)
 
     # Run the OpenGL main loop
     glutMainLoop()
