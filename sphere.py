74d09b97c439f857cff81a4b74ca4b7d87d4d6dd
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-04-25 14:36:32 +0530

    Added DOX

--- sphere.py
+++ sphere.py
@@ -1,8 +1,9 @@
+# author: Somsubhra Bairi (201101056)
+
 # Python imports
-from sys import exit
 from math import *
 
-# OpenGL imports
+# OpenGL imports for python
 try:
     from OpenGL.GL import *
     from OpenGL.GLU import *
@@ -10,7 +11,7 @@ try:
 except:
     print "OpenGL wrapper for python not found"
 
-
+# Last time when sphere was re-displayed
 last_time = 0
 
 
@@ -19,44 +20,86 @@ class Sphere:
 
     # Constructor for the sphere class
     def __init__(self, radius):
+
+        # Radius of sphere
         self.radius = radius
+
+        # Number of latitudes in sphere
         self.lats = 100
+
+        # Number of longitudes in sphere
         self.longs = 100
+
         self.user_theta = 0
         self.user_height = 0
+
+        # Direction of light
         self.direction = [0.0, 2.0, -1.0, 1.0]
+
+        # Intensity of light
         self.intensity = [0.7, 0.7, 0.7, 1.0]
+
+        # Intensity of ambient light
         self.ambient_intensity = [0.3, 0.3, 0.3, 1.0]
+
+        # The surface type(Flat or Smooth)
         self.surface = GL_FLAT
 
+    # Initialize
     def init(self):
+
+        # Set background color to black
         glClearColor(0.0, 0.0, 0.0, 0.0)
+
         self.compute_location()
+
+        # Set OpenGL parameters
         glEnable(GL_DEPTH_TEST)
+
+        # Enable lighting
         glEnable(GL_LIGHTING)
+
+        # Set light model
         glLightModelfv(GL_LIGHT_MODEL_AMBIENT, self.ambient_intensity)
+
+        # Enable light number 0
         glEnable(GL_LIGHT0)
+
+        # Set position and intensity of light
         glLightfv(GL_LIGHT0, GL_POSITION, self.direction)
         glLightfv(GL_LIGHT0, GL_DIFFUSE, self.intensity)
+
+        # Setup the material
         glEnable(GL_COLOR_MATERIAL)
         glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE)
 
+    # Compute location
     def compute_location(self):
         x = 2 * cos(self.user_theta)
         y = 2 * sin(self.user_theta)
         z = self.user_height
         d = sqrt(x * x + y * y + z * z)
 
+        # Set matrix mode
         glMatrixMode(GL_PROJECTION)
+
+        # Reset matrix
         glLoadIdentity()
         glFrustum(-d * 0.5, d * 0.5, -d * 0.5, d * 0.5, d - 1.1, d + 1.1)
+
+        # Set camera
         gluLookAt(x, y, z, 0, 0, 0, 0, 0, 1)
 
     # Display the sphere
     def display(self):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        # Set color to white
         glColor3f(1.0, 1.0, 1.0)
+
+        # Set shade model
         glShadeModel(self.surface)
+
         self.draw()
         glutSwapBuffers()
 
@@ -71,6 +114,7 @@ class Sphere:
             z1 = sin(lat1)
             zr1 = cos(lat1)
 
+            # Use Quad strips to draw the sphere
             glBegin(GL_QUAD_STRIP)
 
             for j in range(0, self.longs + 1):
@@ -84,15 +128,22 @@ class Sphere:
 
             glEnd()
 
+    # Keyboard controller for sphere
     def special(self, key, x, y):
+
+        # Scale the sphere up or down
         if key == GLUT_KEY_UP:
             self.user_height += 0.1
         if key == GLUT_KEY_DOWN:
             self.user_height -= 0.1
+
+        # Rotate the cube
         if key == GLUT_KEY_LEFT:
             self.user_theta += 0.1
         if key == GLUT_KEY_RIGHT:
             self.user_theta -= 0.1
+
+        # Toggle the surface
         if key == GLUT_KEY_F1:
             if self.surface == GL_FLAT:
                 self.surface = GL_SMOOTH
@@ -102,6 +153,7 @@ class Sphere:
         self.compute_location()
         glutPostRedisplay()
 
+    # The idle callback
     def idle(self):
         global last_time
         time = glutGet(GLUT_ELAPSED_TIME)
@@ -110,6 +162,7 @@ class Sphere:
             last_time = time
             glutPostRedisplay()
 
+    # The visibility callback
     def visible(self, vis):
         if vis == GLUT_VISIBLE:
             glutIdleFunc(self.idle)
